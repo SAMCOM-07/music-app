6423fded6c83c27d8782887d61516305b4f740ba
Samuel <160482834+SAMCOM-07@users.noreply.github.com>
2025-08-28 18:35:49 +0100

    Update Song.jsx

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -17,7 +17,7 @@ const Song = ({ song, handleSongChange }) => {
           
             className={`${
               currentSong && currentSong.id === id
-                ? "text-blue-300 text-2xl"
+                ? "text-blue-300 text-[22px]"
                 : " text-white"
             } font-bold max-w-[200px] text-xl truncate hover:scale-105 transition-all duration-200`}
           >
